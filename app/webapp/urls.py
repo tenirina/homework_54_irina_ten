68079a024584706631aa327282ac86e92f0e1fd3
Irina Ten <virhot17@gmail.com>
2022-09-26 22:53:43 +0600

    Add
    Добавлена страница детального просмотра продукта.
    Добавлены product.html, product.py.
    Внесены изменения urls.py, style.css

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from webapp.view.base import products_view
+from webapp.view.product import product_view
 
 urlpatterns = [
     path("", products_view, name='products'),
-    path("products/", products_view, name='products')
+    path("products/", products_view, name='products'),
+    path("products/<int:pk>", product_view, name="product")
 ]
---
+++ app/webapp/view/product.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, get_object_or_404
+
+from webapp.models import Product
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product.html', context={'product': product})
